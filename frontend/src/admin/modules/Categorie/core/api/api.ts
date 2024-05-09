9be0ecf1783d5e68beff2279d85d4f3f5f2dbe35
lambou-rufin <lambonirinarufin@gmail.com>
2024-05-10 13:06:37 +1400

    IMPROVE - added category API

--- frontend/src/admin/modules/Categorie/core/api/api.ts
+++ frontend/src/admin/modules/Categorie/core/api/api.ts
@@ -2,41 +2,24 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ICategory } from '../models';
 import { baseWithFormData } from '../../../../../api/api';
 
-export const fetchCategory = createAsyncThunk<ICategory[]>('category/fetchCategory', async () => {
-  try {
-    const response = await baseWithFormData.get<any>(`/api/category/paginated?per_page=50&page=1`);
-    return response.data.data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des catégories :', error);
-    throw error;
-  }
+
+export const fetchCategory = createAsyncThunk<ICategory[]>('produits/fetchCategory', async () => {
+  const response = await baseWithFormData.get<any>(`/api/category/paginated?per_page=50&page=1`);
+  return response.data.data;
 });
 
 export const addCategory = createAsyncThunk<ICategory, ICategory>('category/addCategory', async (category) => {
-  try {
     const response = await baseWithFormData.post<ICategory>(`/api/category`, category);
     return response.data;
-  } catch (error) {
-    console.error('Erreur lors de l\'ajout de la catégorie :', error);
-    throw error;
-  }
 });
 
 export const updateCategory = createAsyncThunk<ICategory, ICategory>('category/updateCategory', async (category) => {
-  try {
     const response = await baseWithFormData.post<ICategory>(`/api/category/${category.id}`, category);
     return response.data;
-  } catch (error) {
-    console.error('Erreur lors de la mise à jour de la catégorie :', error);
-    throw error;
-  }
 });
 
 export const deleteCategory = createAsyncThunk<void, number>('category/deleteCategory', async (id) => {
-  try {
-    await baseWithFormData.delete(`/api/category/${id}`);
-  } catch (error) {
-    console.error('Erreur lors de la suppression de la catégorie :', error);
-    throw error;
+  const response = await baseWithFormData.delete(`/api/category/${id}`);
+  return response.data;
   }
-});
+);
